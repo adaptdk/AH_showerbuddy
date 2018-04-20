00e3e7b17763f54d4fb0fd2193906c4c739ff2d6
nielsiano <niels@adapt.dk>
2018-04-20 15:35:45 +0200

    adds routes

--- api/routes/web.php
+++ api/routes/web.php
@@ -2,15 +2,13 @@
 
 /*
 |--------------------------------------------------------------------------
-| Application Routes
+| Showerbuddy API routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
 */
+$router->get('/api/state', 'ShowerbuddyController@currentState');
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
+/*
+|--------------------------------------------------------------------------
+| Showerbuddy web routes
+|--------------------------------------------------------------------------
+*/
