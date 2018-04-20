6b8277b79a96a2bcc34fd5ff3a3319b10217acc6
Casper Juel <casper@adapt.dk>
2018-04-20 20:23:24 +0200

    made it prettier

--- api/resources/views/showerbuddy.php
+++ api/resources/views/showerbuddy.php
@@ -1,7 +1,15 @@
 <html>
-    <body>
-        <h1>
-            Der er fandme <?php echo \App\OccupiedState::isOccupied() ? 'optaget' : 'ledigt'; ?>!
+    <body style="display:flex;justify-content: center;align-items: center;background-color:<?php echo \App\OccupiedState::isOccupied() ? 'red' : 'green' ?>">
+    <style>
+      body {font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";}
+    </style>
+    <script>
+        setTimeout(() => {
+            location.reload();
+        }, 10000);
+    </script>
+        <h1 style="font-weight:normal;text-align:center; color:white;">
+        Så der fandeme <b><?php echo \App\OccupiedState::isOccupied() ? 'Optaget' : 'Ledigt' ?></b>
         </h1>
     </body>
 </html>
